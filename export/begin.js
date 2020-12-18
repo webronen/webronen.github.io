b71ff6972c5c7912433bfcf52e065ef2d8bfd731
webronen <jarno.piironen@hotmail.com>
2020-12-18 11:30:18 +0000

    Update begin.js

--- export/begin.js
+++ export/begin.js
@@ -7,8 +7,10 @@ const setup = ( setup ) => {
   if( typeof setup !== 'function' ) return console.error( 'Invalid argument type in setup. Valid type is function.' );
   
   event( () => {
+    
     setup();
     if( loopIsDefined ) begin();
+    
   }, window, 'load', false );
   
   setupIsDefined = true;
@@ -20,6 +22,8 @@ const loop = ( loop, rate ) => {
   if( loopIsDefined ) return console.error( 'Loop is already defined.' );
   if( typeof loop !== 'function' ) return console.error( 'Invalid first argument type in loop. Valid type is function.' );
   if( typeof rate !== 'number' ) return console.error( 'Invalid second argument type in loop. Valid type is number.' );
+  if( !window.setTimeout ) console.error( 'Your browser does not support timeout method.' );
+  if( !window.requestAnimationFrame ) console.error( 'Your browser does not support animation frames.' );
   
   rate = rate < 1 ? 1: rate > 30 ? 30: rate;
   const Tms = ~~( ( 1000 / rate ) + .5 );
@@ -35,15 +39,19 @@ const loop = ( loop, rate ) => {
       pastRenderTime = window.performance.now();
       
       loop( {
+        
         screenFrameCount: screenFrameCount,
         screenFrameRate: ~~( ( 1000 / ( window.performance.now() - pastScreenTime ) ) + .5 ),
         renderFrameCount: renderFrameCount,
         renderFrameRate: renderFrameRate
+        
       } );
       
       renderFrameCount = window.setTimeout( () => {
+        
         renderFrameRate = ~~( ( 1000 / ( window.performance.now() - pastRenderTime ) ) + .5 );
         drawNewFrame = true;
+        
       }, Tms );
       
     }
