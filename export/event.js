eece7e2693a090335565ceb3c03c54792ea45819
Jarno Piironen <69757212+webronen@users.noreply.github.com>
2021-02-01 02:17:10 +0200

    Update event.js

--- export/event.js
+++ export/event.js
@@ -1,9 +1,9 @@
 const event = ( listener, target, type, usecapture ) => {
-  if( typeof listener !== 'function' ) console.error( 'Invalid first argument type in event. Valid type is function.' );
-  if( typeof target !== 'object' ) console.error( 'Invalid second argument type in event. Valid type is object.' );
-  if( typeof type !== 'string' ) console.error( 'Invalid third argument type in event. Valid type is string.' );
-  if( typeof usecapture !== 'boolean' ) console.error( 'Invalid fourth argument type in event. Valid type is boolean.' );
-  if( !window.addEventListener ) console.error( 'Your browser does not support event listeners.' );
+  if( typeof listener !== 'function' ) return console.error( 'Invalid first argument type in event. Valid type is function.' );
+  if( typeof target !== 'object' ) return console.error( 'Invalid second argument type in event. Valid type is object.' );
+  if( typeof type !== 'string' ) return console.error( 'Invalid third argument type in event. Valid type is string.' );
+  if( typeof usecapture !== 'boolean' ) return console.error( 'Invalid fourth argument type in event. Valid type is boolean.' );
+  if( !window.addEventListener ) return console.error( 'Your browser does not support event listeners.' );
   target.addEventListener( type, listener, usecapture );
 };
 
