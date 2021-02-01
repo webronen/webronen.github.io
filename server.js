ab64752b37e415cf9b09eb65199a0f7f7647924f
webronen <jarno.piironen@hotmail.com>
2021-02-01 03:33:09 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-const http = require('http');
+const http = require( 'http' );
+const path = require( 'path' );
+const fs = require( 'fs' );
 
 const hostname = '127.0.0.1';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
+const mime = [];
+mime[0] = [ '.html','text/html' ];
+mime[1] = [ '.css','text/css' ];
+mime[2] = [ '.js', 'text/javascript' ];
+
+const server = http.createServer( ( req, res ) => {
+  
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
+  res.setHeader( 'Content-Type', 'text/plain' );
+  res.end( 'Hello World!' );
+  
+} );
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+  
+  console.log( `Server running at http://${hostname}:${port}/` );
+  
+} );
